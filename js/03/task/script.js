124ff231df35a738d68a9af600d7ac7e6a5ba22c
berneau <bernhard.steger@gmail.com>
2019-11-12 09:05:59 +0100

    review JS03

--- js/03/task/script.js
+++ js/03/task/script.js
@@ -1,3 +1,23 @@
-var hallo ="Hallo";
-var welt ="Welt";
+var variable1 ="Hallo";
+var variable2 ="Welt";
 console.log("Hallo"+" "+"Welt");
+
+// so wie du es jetzt geschrieben hast funktioniert es zwar aber die variablen
+// werden eigentlich nicht verwendet. Du speicherst den wert "Hallo" in die
+// variable hallo, aber du gibst dann nicht den Wert von der Variable aus,
+// sondern einfach nochmal den Text "Hallo".
+// wenn du die ersten 2 Zeilen rauslöschst wirst du sehen dass es immer noch
+// funktioniert.
+// ich benenne dir die variablen und damit es leichter zu verstehen ist :)
+
+// ich versuchs nochmal zu erklären - hoffentlich diesmal verständlicher :D
+// Variablen sind wie Container, man kann Sachen reintun und immer wieder
+// nachschauen was drinnen ist.
+
+// des wort "var" sagt dem programm, dass du eine neue Variable erstellst
+// nach dem "var" kommt der Name der Variable (zb. blaueBox)
+// mit dem "=" sagst du was in diese Variable gespeichert werden soll (zb. eine
+// Zahl oder Text)
+// Um zu einem späteren Zeitpunkt diesen Text wieder zu bekommen, kann man das
+// mit dem befehl console.log(blaueBox) machen.
+// WICHTIG -> variablen verwendet man ohne Anführungszeichen!
